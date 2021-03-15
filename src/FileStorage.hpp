c42cdf106008e4a0bfb40e8ece92ac9a791eba2e
Eric Aubin <eric.aubin@olympusndt.com>
2021-03-15 15:25:24 -0400

    write ascan data.

--- src/FileStorage.hpp
+++ src/FileStorage.hpp
@@ -14,17 +14,34 @@ public:
     {
       hid_t fileId = H5Fcreate(FILENAME, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
 
-      hsize_t dims[3] = { 256, 512, 4096 }; // Scan, Index, Ultrasound
+      const uint16_t height(10);
+      const uint16_t width(20);
+      const uint16_t depth(30);
+      const hsize_t dims[3] = { height, width, depth };
       hid_t spaceId = H5Screate_simple(3, dims, NULL);
       hid_t dsetId = H5Dcreate(fileId, "DS1", H5T_STD_I16LE, spaceId, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 
       H5D_space_status_t spaceStatus;
       herr_t status = H5Dget_space_status(dsetId, &spaceStatus);
 
-      std::vector<short> wdata((short)1 << 29, 1);
-      //memset(wdata, 0, sizeof(wdata[0][0][0]) * 512 * 1024 * 2048);
+      std::vector<short> ascanData(depth);
+      std::vector<short> dataSource(6000, 0);
 
-      status = H5Dwrite(dsetId, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT, wdata.data());
+      size_t memOffet{};
+      for (uint16_t y{}; y < height; y++)
+      {
+        for (uint16_t x{}; x < width; x++)
+        {
+          uint16_t xy = width * y + x;
+          std::iota(std::begin(ascanData), std::end(ascanData), xy);
+
+          size_t ascanSize = ascanData.size();
+          memcpy(dataSource.data() + memOffet, ascanData.data(), ascanSize * sizeof(uint16_t));
+          memOffet += ascanSize;
+        }
+      }
+
+      status = H5Dwrite(dsetId, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT, dataSource.data());
 
       status = H5Dget_space_status(dsetId, &spaceStatus);
       hsize_t storageSize = H5Dget_storage_size(dsetId);
